606fb7e313d6db7775d32025452069377f902057
chad <chad.but@gmail.com>
2014-02-21 16:38:11 +0800

    add testcase

---
+++ converter/src/main/java/org/search/bean/converter/Exception/ConvertException.java
@@ -0,0 +1,8 @@
+package org.search.bean.converter.Exception;
+
+public class ConvertException extends Exception{
+	private static final long serialVersionUID = 1L;
+	ConvertException(String msg){
+		super(msg);
+	}
+}
